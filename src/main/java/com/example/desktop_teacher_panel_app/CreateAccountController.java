d2650dde533be2b8067abbe47c8791876a0de8a8
zeeshantaj <zeeshantaj.zt@gmail.com>
2023-11-11 23:35:16 +0500

    second commit

--- src/main/java/com/example/desktop_teacher_panel_app/CreateAccountController.java
+++ src/main/java/com/example/desktop_teacher_panel_app/CreateAccountController.java
@@ -1,4 +1,7 @@
 package com.example.desktop_teacher_panel_app;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -35,9 +38,29 @@ public class CreateAccountController {
         stage.setTitle("User Login");
         //stage.getIcons().add(new Image("/assets/pngegg.png"));
         stage.show();
+        initializedFirebase();
     }
+
+    private void initializedFirebase() {
+        try {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(
+                    CreateAccountController.class.getResourceAsStream("/google_gson/google-services.json")); // Update the path
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(credentials)
+                    .setDatabaseUrl("https://your-project-id.firebaseio.com") // Replace with your database URL
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @FXML
     private void signInUser(){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
         String  em = email.getText();
         String pass = password.getText();
         if (em.isEmpty() && pass.isEmpty()){
@@ -45,20 +68,21 @@ public class CreateAccountController {
         }
         else {
             try {
-                GoogleCredentials credentials = GoogleCredentials.fromStream(
-                        CreateAccountController.class.getResourceAsStream("/google_gson/google-services.json")); // Update the path
-
-                FirebaseOptions options = new FirebaseOptions.Builder()
-                        .setCredentials(credentials)
-                        .setDatabaseUrl("https://your-project-id.firebaseio.com") // Replace with your database URL
-                        .build();
-
-                FirebaseApp.initializeApp(options);
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                UserRecord userRecord = FirebaseAuth.getInstance().getUserByEmail(em);
             }
+            catch (FirebaseAuthException e){
+                try {
+                    UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                            .setEmail(em)
+                            .setPassword(pass);
 
+                    UserRecord userRecord = FirebaseAuth.getInstance().createUser(request);
+
+                }
+                catch (FirebaseAuthException createException){
+                    System.out.println("ERROR "+createException.getLocalizedMessage());
+                }
+            }
         }
     }
 
