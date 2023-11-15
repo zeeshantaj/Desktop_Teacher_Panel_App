6f69404463c04758d6f84d43229e8ddb95d0869d
zeeshantaj <zeeshantaj.zt@gmail.com>
2023-11-16 00:11:41 +0500

    fifth7 commit

--- src/main/java/com/example/desktop_teacher_panel_app/ClassHistoryController.java
+++ src/main/java/com/example/desktop_teacher_panel_app/ClassHistoryController.java
@@ -1,9 +1,12 @@
 package com.example.desktop_teacher_panel_app;
 
+import com.example.desktop_teacher_panel_app.Model.DetailsClass;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -11,37 +14,39 @@ import javafx.scene.layout.VBox;
 
 public class ClassHistoryController {
 
-    private ObservableList<String> items;
-    private ListView<String> listView;
-    private TextField newItemField;
-    private int editIndex = -1; // Initialize to -1
-    public void initialize() {
-        items = FXCollections.observableArrayList();
-        listView = new ListView<>(items);
-        newItemField = new TextField();
-        Button addButton = new Button("Add");
-        Button removeButton = new Button("Remove");
-        Button editButton = new Button("Edit");
+    @FXML
+    ListView<DetailsClass> meetingListView = new ListView<>();
 
-        addButton.setOnAction(event -> {
-            String newItem = newItemField.getText();
-            if (!newItem.isEmpty()) {
-                if (editIndex != -1) {
-                    items.set(editIndex, newItem); // Edit existing item
-                    editIndex = -1; // Reset edit index
+    public void initialize() {
+        // Sample meetings
+
+        DetailsClass detailsClass = new DetailsClass("zeeshan taj","math","BSSE","new Building conference 1"
+                ,"geometry","123");
+        DetailsClass detailsClass1 = new DetailsClass("zeeshan taj","math","BSSE","new Building conference 1"
+                ,"geometry","11123");
+
+
+// Add meetings to the ListView
+        meetingListView.getItems().addAll(detailsClass,detailsClass1);
+
+// Create a cell factory to display items in multiple columns
+        meetingListView.setCellFactory(param -> new ListCell<>() {
+            @Override
+            protected void updateItem(DetailsClass modelClass, boolean empty) {
+                super.updateItem(modelClass, empty);
+
+                if (empty || modelClass == null) {
+                    setText(null);
                 } else {
-                    items.add(newItem); // Add new item
+                    // Display meeting details in multiple columns
+                    setText("Teacher: " + modelClass.getTeacherName() +
+                            ", Class: " + modelClass.getClassLocation() +
+                            ", Topic: " + modelClass.getTodayTopic() +
+                            ", Subject: " + modelClass.getSubjectName() +
+                            ", Minutes: " + modelClass.getMinutes());
                 }
-                newItemField.clear();
             }
         });
-        HBox inputBox = new HBox(10);
-        inputBox.getChildren().addAll(newItemField, addButton, removeButton, editButton);
-
-        VBox root = new VBox(10);
-        root.getChildren().addAll(inputBox, listView);
-
-        Scene scene = new Scene(root, 400, 300);
 
     }
 }
--- src/main/java/com/example/desktop_teacher_panel_app/Model/DetailsClass.java
+++ src/main/java/com/example/desktop_teacher_panel_app/Model/DetailsClass.java
@@ -2,4 +2,69 @@ package com.example.desktop_teacher_panel_app.Model;
 
 public class DetailsClass {
 
+    private String teacherName,subjectName,department,classLocation,todayTopic,keyText,minutes;
+    public DetailsClass(String teacherName, String subjectName, String department, String classLocation, String todayTopic,String minutes) {
+        this.teacherName = teacherName;
+        this.subjectName = subjectName;
+        this.department = department;
+        this.classLocation = classLocation;
+        this.todayTopic = todayTopic;
+        this.minutes = minutes;
+    }
+
+    public String getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(String minutes) {
+        this.minutes = minutes;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName=teacherName;
+    }
+
+    public String getTeacherName(){
+        return teacherName;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getClassLocation() {
+        return classLocation;
+    }
+
+    public void setClassLocation(String classLocation) {
+        this.classLocation = classLocation;
+    }
+
+    public String getTodayTopic() {
+        return todayTopic;
+    }
+
+    public void setTodayTopic(String todayTopic) {
+        this.todayTopic = todayTopic;
+    }
+
+    public String getKeyText() {
+        return keyText;
+    }
+
+    public void setKeyText(String keyText) {
+        this.keyText = keyText;
+    }
 }
